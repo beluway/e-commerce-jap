955488c755033563b154c80fbef5deff22522393
Beluway <morenosuarezmariabelen@gmail.com>
2025-09-11 17:02:18 -0300

    Co-authored-by: akihito <akihito2000@users.noreply.github.com> Co-authored-by: BenjaminGuardone <BenjaminGuardone@users.noreply.github.com> Co-authored-by: francisca <francisca-volpe@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -1,7 +1,6 @@
 import { getUsuario } from "./clases/Usuario.js";
 
-let productsArray = [];
-
+    let productsArray = [];
 function showProductsList(array){ //crea una fila por cada producto.
     let htmlContentToAppend = "";
 
@@ -15,11 +14,19 @@ function showProductsList(array){ //crea una fila por cada producto.
             <td class="price">${product.currency}${product.cost}</td>
          </tr>`;
     }
+    
     document.getElementById("products-list").innerHTML = htmlContentToAppend;
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData("https://japceibal.github.io/emercado-api/cats_products/101.json")
+
+
+    //obtengo el id de la categoría actual del localStorage
+    let catID = localStorage.getItem('catID');
+
+     PRODUCTS_URL+catID+EXT_TYPE
+    //la incluyo en la URL del Json de forma dinámica con un string literal
+    getJSONData(`https://japceibal.github.io/emercado-api/cats_products/${catID}.json`)
     .then(function(resultObj){
         if (resultObj.status === "ok"){
             productsArray = resultObj.data.products;
@@ -31,3 +38,83 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     userNameElement.textContent = usuario.email;
 });
+
+//obtengo los elementos del DOM
+const filtroAsc = document.getElementById("asc");
+const filtroDesc = document.getElementById("desc");
+const filtroRel = document.getElementById("rel");
+
+/*filtroAsc.addEventListener('click',()=>{
+    productsArray.sort((a,b)=>b.cost-a.cost);
+    showProductsList(productsArray);
+});
+
+filtroDesc.addEventListener('click',()=>{
+    productsArray.sort((a,b)=>a.cost-b.cost);
+    showProductsList(productsArray);
+});
+
+filtroRel.addEventListener('click',()=>{
+    productsArray.sort((a,b)=>b.soldCount-a.soldCount);
+    showProductsList(productsArray);
+});*/
+
+/*function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.name < b.name ){ return -1; }
+            if ( a.name > b.name ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_NAME){
+        result = array.sort(function(a, b) {
+            if ( a.name > b.name ){ return -1; }
+            if ( a.name < b.name ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.productCount);
+            let bCount = parseInt(b.productCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+} */
+
+    //PENDIENTE problema clicks
+function sortCat(criteria){
+
+    switch(criteria){
+        case "asc":
+        productsArray.sort((a,b)=>b.cost-a.cost);
+    showProductsList(productsArray);
+            break;
+        case "desc":
+        productsArray.sort((a,b)=>a.cost-b.cost);
+    showProductsList(productsArray);
+            break;
+            case "rel":
+            productsArray.sort((a,b)=>b.soldCount-a.soldCount);
+        showProductsList(productsArray);
+        break;
+    }
+}
+
+    filtroAsc.addEventListener("click", function(){
+        sortCat("asc");
+    });
+    filtroDesc.addEventListener("click", function(){
+        sortCat("desc");
+    });
+    filtroRel.addEventListener("click", function(){
+        sortCat("rel");
+    });
+
+    
