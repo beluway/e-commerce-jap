daac85d8c042b76931785e48f8bca2e7bdaf9a4e
Beluway <morenosuarezmariabelen@gmail.com>
2025-09-13 12:09:46 -0300

    -Terminé de de adecuar el comportamiento de los botones de ordenamiento de la lista de productos. -Completé el desafíate agregando el js para buscar en tiempo real.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,30 @@
 import { getUsuario } from "./clases/Usuario.js";
 
-    let productsArray = [];
+//inicializo el array de products vacío
+let productsArray = [];
+
+//carrgado del json de productos
+document.addEventListener("DOMContentLoaded", function(e){
+
+
+    //obtengo el id de la categoría actual del localStorage
+    let catID = localStorage.getItem('catID');
+    //la incluyo en la URL del Json de forma dinámica con un string literal
+    //idea 1: `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`
+     
+    getJSONData(PRODUCTS_URL+catID+EXT_TYPE)
+    .then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data.products;
+            showProductsList(productsArray);
+        }
+    })
+    const usuario = getUsuario();
+    const userNameElement = document.getElementById("userName");
+
+    userNameElement.textContent = usuario.email;
+});
+
 function showProductsList(array){ //crea una fila por cada producto.
     let htmlContentToAppend = "";
 
@@ -18,96 +42,35 @@ function showProductsList(array){ //crea una fila por cada producto.
     document.getElementById("products-list").innerHTML = htmlContentToAppend;
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-
-
-    //obtengo el id de la categoría actual del localStorage
-    let catID = localStorage.getItem('catID');
-
-    //idea 1: `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`
-     
-    //la incluyo en la URL del Json de forma dinámica con un string literal
-    getJSONData(PRODUCTS_URL+catID+EXT_TYPE)
-    .then(function(resultObj){
-        if (resultObj.status === "ok"){
-            productsArray = resultObj.data.products;
-            showProductsList(productsArray);
-        }
-    })
-    const usuario = getUsuario();
-    const userNameElement = document.getElementById("userName");
-
-    userNameElement.textContent = usuario.email;
-});
-
 //obtengo los elementos del DOM
 const filtroAsc = document.getElementById("asc");
 const filtroDesc = document.getElementById("desc");
 const filtroRel = document.getElementById("rel");
 
-/*filtroAsc.addEventListener('click',()=>{
-    productsArray.sort((a,b)=>b.cost-a.cost);
-    showProductsList(productsArray);
-});
-
-filtroDesc.addEventListener('click',()=>{
-    productsArray.sort((a,b)=>a.cost-b.cost);
-    showProductsList(productsArray);
-});
-
-filtroRel.addEventListener('click',()=>{
-    productsArray.sort((a,b)=>b.soldCount-a.soldCount);
-    showProductsList(productsArray);
-});*/
-
-/*function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.productCount);
-            let bCount = parseInt(b.productCount);
-
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
-} */
-
-    //PENDIENTE problema clicks
 function sortCat(criteria){
+
+    //creo una copia del array original para no modificarlo repetidamente
+    //los tres puntos se llaman operador spread y crean una copia superficial del array
+    let arrayOrdenado = [...productsArray]; 
 
     switch(criteria){
         case "asc":
-        productsArray.sort((a,b)=>b.cost-a.cost);
-    showProductsList(productsArray);
+            //de menor a mayor precio
+        arrayOrdenado.sort((a,b)=>a.cost-b.cost);
             break;
         case "desc":
-        productsArray.sort((a,b)=>a.cost-b.cost);
-    showProductsList(productsArray);
+            //de mayor a menor precio
+        arrayOrdenado.sort((a,b)=>b.cost-a.cost);
             break;
             case "rel":
-            productsArray.sort((a,b)=>b.soldCount-a.soldCount);
-        showProductsList(productsArray);
-        break;
+                //de más cantidad de vendidos a menos
+            arrayOrdenado.sort((a,b)=>b.soldCount-a.soldCount);
+        break; 
     }
+    showProductsList(arrayOrdenado);
 }
 
+//listeners de los botones de ordenamiento
     filtroAsc.addEventListener("click", function(){
         sortCat("asc");
     });
@@ -117,5 +80,29 @@ function sortCat(criteria){
     filtroRel.addEventListener("click", function(){
         sortCat("rel");
     });
+
+    //buscador en tiempo real
+    const buscador = document.getElementById("buscador");
+
+    buscador.addEventListener("input", function(e){
+    const busqueda = e.target.value.toLowerCase();
+    
+    const productsFiltro = productsArray.filter(product =>
+            //incluye el producto si el nombre contiene lo que se escribió en el buscador
+            product.name.toLowerCase().includes(busqueda) ||
+            //incluye el producto si la descripción contiene lo escrito en el buscador
+            product.description.toLowerCase().includes(busqueda)
+        );
+        //llamo a la función que muestra los productos pasandole el array filtrado
+        showProductsList(productsFiltro);
+        //si no hay coincidencias, muestro un mensaje en un p de color rojo
+        if(productsFiltro.length==0 ||(product.name.toLowerCase().includes(busqueda))){
+            document.getElementById("products-list").innerHTML = "<p style='color:red;'>Ningún producto coincide con la búsqueda</p>";
+        }
+    });
+
+    
+
+
 
 
